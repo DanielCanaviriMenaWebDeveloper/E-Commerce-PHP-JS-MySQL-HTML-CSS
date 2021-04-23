1b4712dfec0ae324e2eea196bafb6ec2b5c46438
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-04-22 23:49:55 -0400

    Validando campos

--- Views/register.php
+++ Views/register.php
@@ -16,6 +16,34 @@
         <link rel="stylesheet" href="../Util/css/toastr.min.css">
         <link rel="shortcut icon" href="#">
     </head>
+    <!-- Modal -->
+    <div class="modal fade" id="terminos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h5 class="card-title">Terminos y condiciones</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>No vendemos tus datos personales a los anunciantes ni compartimos información que te identifique directamente (como tu nombre, dirección de correo electrónico u otra información de contacto) con los anunciantes, a menos que nos des tu permiso expreso. </li>
+
+                            <li>Trabajamos continuamente para optimizar nuestros servicios y desarrollar nuevas funciones que mejoren nuestros Productos para ti y nuestra comunidad.</li>
+
+                            <li>Si determinamos que incumpliste nuestras Condiciones o Políticas en reiteradas oportunidades o de una manera notoria o grave, incluidas en particular nuestras Normas comunitarias, podemos suspender o inhabilitar definitivamente el acceso a tu cuenta.</li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary btn-block" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.Modal -->
     <body class="hold-transition login-page">
         <div class="mt-5">
             <div class="login-logo">
@@ -26,34 +54,78 @@
             <div class="card">
                 <div class="card-body login-card-body">
                     <p class="login-box-msg">Regístrarse</p>
-                    <form id="quickForm">
+
+                    <form id="form-register">
+
                         <div class="card-body">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
-                            <div class="form-group mb-0">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
-                                    <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms of service</a>.</label>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label for="username">Nombre de Usuario</label>
+                                        <input type="text" name="username" class="form-control" id="username" placeholder="Ingrese nombre de usuario">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="pass">Contraseña</label>
+                                        <input type="password" name="pass" class="form-control" id="pass" placeholder="Ingrese contraseña">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="nombres">Nombre(s)</label>
+                                        <input type="text" name="nombres" class="form-control" id="nombres" placeholder="Ingrese nombre(s)">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="dni">DNI</label>
+                                        <input type="text" name="dni" class="form-control" id="dni" placeholder="Ingrese DNI">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="telefono">Teléfono</label>
+                                        <input type="text" name="telefono" class="form-control" id="telefono" placeholder="Ingrese Teléfono">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="pass-repeat">Repetir Contraseña</label>
+                                        <input type="password" name="pass_repeat" class="form-control" id="pass_repeat" placeholder="Ingrese de nuevo su contraseña">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="apellidos">Apellido(s)</label>
+                                        <input type="text" name="apellidos" class="form-control" id="apellidos" placeholder="Ingrese apellido(s)">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="email">Correo Electrónico</label>
+                                        <input type="text" name="email" class="form-control" id="email" placeholder="Ingrese correo electrónico">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-12">
+                                    <div class="form-group mb-0">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" name="terms" class="custom-control-input" id="terms">
+                                            <label class="custom-control-label" for="terms">Estoy de acuerdo con los <a href="#" data-toggle="modal" data-target="#terminos">Terminos de Servicio</a>.</label>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <!-- /.card-body -->
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="card-footer text-center">
+                            <button type="submit" class="btn btn-lg bg-gradient-primary">Registrarme</button>
                         </div>
+                        <!-- /.card-footer -->
                     </form>
                 </div>
                 <!-- /.login-card-body -->
             </div>
         </div>
         
-
         <!-- jQuery -->
         <script src="../Util/js/jquery.min.js"></script>
         <!-- Bootstrap 4 -->
@@ -71,8 +143,28 @@
                         alert( "Form successful submitted!" );
                     }
                 });
-                $('#quickForm').validate({
+                $('#form-register').validate({
                     rules: {
+                        username: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 20
+                        },
+                        pass: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 20
+                        },
+                        pass_repeat: {
+                            required: true,
+                            equalTo: "#pass"
+                        },
+                        nombres: {
+                            required: true,
+                        },
+                        apellidos: {
+                            required: true,
+                        },
                         email: {
                             required: true,
                             email: true,
@@ -86,6 +178,25 @@
                         },
                     },
                     messages: {
+                        username: {
+                            required: "* Este campo es obligatorio",
+                            minlength: "* El nombre de usuario debe tener como minimo 8 caracteres",
+                            maxlength: "* El nombre de usuario debe tener como maximo 20 caracteres"
+                        },
+                        pass: {
+                            required: "* Este campo es obligatorio",
+                            minlength: "* La contraseña debe tener como minimo 8 caracteres",
+                            maxlength: "* La contraseña debe tener como maximo 20 caracteres"
+                        },
+                        pass_repeat: {
+                            required: "* Este campo es obligatorio",
+                            equalTo: "* Ingrese una contraseña igual a la anterior"
+                        },
+                        nombres: {
+                            required: "* Este campo es obligatorio",
+                            minlength: "* La contraseña debe tener como minimo 8 caracteres",
+                            maxlength: "* La contraseña debe tener como maximo 20 caracteres"
+                        },
                         email: {
                             required: "Please enter a email address",
                             email: "Please enter a vaild email address"
@@ -103,9 +214,11 @@
                     },
                     highlight: function (element, errorClass, validClass) {
                         $(element).addClass('is-invalid');
+                        $(element).removeClass('is-valid');
                     },
                     unhighlight: function (element, errorClass, validClass) {
                         $(element).removeClass('is-invalid');
+                        $(element).addClass('is-valid');
                     }
                 });
             });
